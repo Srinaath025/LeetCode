a9485b4ab51442016fc32ab0590a7395abee9265
Srinaath025 <srinaathpk25@gmail.com>
2026-06-04 20:55:16 +0530

    Solved: Koko Eating Bananas in Java

--- LeetCode/Medium/koko-eating-bananas.java
+++ LeetCode/Medium/koko-eating-bananas.java
@@ -1,30 +1,24 @@
-class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int max=0;
-        for(int i=0;i<piles.length;i++)
-        {
-            if(piles[i]>max)
-            {
-                max=piles[i];
-            }
+
+            long r = 0;
         }
 
-        for(int i=1;i<=max;i++)
-        {
-            int r=0;
-            for(int j=0;j<piles.length;j++)
-            {
-                r=r+(piles[j]+i-1)/i;
+        for(int i = 1; i <= max; i++) {
 
+            for(int j = 0; j < piles.length; j++) {
+                r += (piles[j] + i - 1L) / i;
             }
 
-            if(r<=h) 
-            {
+            if(r <= h) {
                 return i;
             }
         }
 
         return max;
-        
     }
 }
+
+        for(int i = 0; i < piles.length; i++) {
+            max = Math.max(max, piles[i]);
+        int max = 0;
+    public int minEatingSpeed(int[] piles, int h) {
+class Solution {
