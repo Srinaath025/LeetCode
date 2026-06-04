5fd53ebf14f7931fb69a7548a7694e70d9ce364c
Srinaath025 <srinaathpk25@gmail.com>
2026-06-04 12:45:15 +0530

    Solved: Longest Consecutive Sequence in java

--- LeetCode/Easy/longest-consecutive-sequence.java
+++ LeetCode/Easy/longest-consecutive-sequence.java
@@ -1,11 +1,12 @@
             {
             if(arr[i]==arr[i-1])
         {
-        for(int  i=1;i<arr.length;i++)
-        int max=0;
+        for(int i=1;i<arr.length;i++)
+        int max=1;
         Arrays.sort(arr);
         int c=1;
 
     public int longestConsecutive(int[] arr) {
 class Solution {
         if(arr.length==1) return 1;
+                continue;
