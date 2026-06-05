1d13cf0a33b5f11442b5e163adbd2d3d097f1cc5
Srinaath025 <srinaathpk25@gmail.com>
2026-06-05 09:19:39 +0530

    Solved: Score of Parentheses in java

--- LeetCode/Medium/score-of-parentheses.java
+++ LeetCode/Medium/score-of-parentheses.java
@@ -1,1 +1,21 @@
-c
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer>st=new Stack<>();
+        char[]ch=s.toCharArray();
+        st.push(0);
+        int count=0;
+        for(char c:ch)
+        {
+            if(c=='(')st.push(0);
+            else
+            {
+                int s1=st.pop();
+                int sc=st.pop();
+                sc+=(s1==0)?1:2*s1;
+                st.push(sc);
+            }
+        }
+
+        return st.pop();
+    }
+}
