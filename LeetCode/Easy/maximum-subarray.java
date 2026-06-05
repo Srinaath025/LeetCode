10461536b1dbbb5a401861f6a47eb3c4d326451c
Srinaath025 <srinaathpk25@gmail.com>
2026-06-05 09:45:05 +0530

    Solved: Maximum Subarray in java

---
+++ LeetCode/Easy/maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+       int sum=0;
+       int max=0;
+       for(int i=0;i<nums.length;i++)
+       {
+        sum=Math.max(nums[i],max+nums[i]);
+        max=Math.max(sum,max);
+       }
+        return max;
+    }
+}
